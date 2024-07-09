db1ad793fd3929fd1cc3355a4fa73b06008540d3
Bruna <brunamata@usp.br>
2024-07-09 20:13:07 -0300

    add tests to sonar coverage

--- src/test/java/dupradosantini/sostoolbackend/services/FilterServiceImplTest.java
+++ src/test/java/dupradosantini/sostoolbackend/services/FilterServiceImplTest.java
@@ -47,4 +47,41 @@ public class FilterServiceImplTest {
 
         assertEquals("Test Filter", createdFilterDto.getName());
     }
+
+    @Test
+    public void testAtivarFiltroSucesso() {
+        Long id = 1l;
+
+        Filter filterOriginal = new Filter(
+                1,
+                "atividade = concluída",
+                false,
+                "atividades"
+        );
+
+        Filter result = new Filter(
+                1,
+                "atividade = concluída",
+                true,
+                "atividades"
+        );
+
+        when(filterRepository.findById(id)).thenReturn(Optional.of(filterOriginal));
+        when(filterRepository.save(any(Filter.class))).thenReturn(result);
+
+       boolean success = filterService.ativarDesativarFiltro(id);
+
+        assertEquals(true, success);
+    }
+
+    @Test
+    public void testAtivarFiltroFalha() {
+        Long id = 2l;
+
+        when(filterRepository.findById(id)).thenReturn(Optional.empty());
+
+        boolean success = filterService.ativarDesativarFiltro(id);
+
+        assertEquals(false, success);
+    }
 }
