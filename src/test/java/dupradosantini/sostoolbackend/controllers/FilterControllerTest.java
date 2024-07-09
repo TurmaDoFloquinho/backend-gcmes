b3040da8d759b9cd184525ab5a9d6155bf0e29e0
afp-lucas <almeidalucas@usp.br>
2024-07-09 15:01:17 -0300

    testando erro de coverage sonar

---
+++ src/test/java/dupradosantini/sostoolbackend/controllers/FilterControllerTest.java
@@ -0,0 +1,46 @@
+package dupradosantini.sostoolbackend.controllers;
+
+import dupradosantini.sostoolbackend.domain.dtos.FilterDto;
+import dupradosantini.sostoolbackend.services.FilterServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class FilterControllerTest {
+
+    @InjectMocks
+    private FilterController filterController;
+
+    @Mock
+    private FilterServiceImpl filterService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testCreateFilter() {
+        FilterDto filterDto = new FilterDto();
+        filterDto.setName("Test Filter");
+        filterDto.setActive(true);
+        filterDto.setType("Type1");
+
+        when(filterService.createFilter(any(FilterDto.class))).thenReturn(filterDto);
+
+        ResponseEntity<FilterDto> response = filterController.createFilter(filterDto);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals("Test Filter", response.getBody().getName());
+    }
+}
